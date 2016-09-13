5382b2b628c5f4d7203eee6625cc45a7b621b0b8
Eliot S <eliotpszw@gmail.com>
2016-09-13 11:57:58 -0400

    Seed File Operational

--- seed.js
+++ seed.js
@@ -20,6 +20,9 @@ name in the environment files.
 var chalk = require('chalk')
 var db = require('./server/db')
 var User = db.model('user')
+var Product = db.model('product')
+var Cart = db.model('cart')
+var Order = db.model('order')
 var Promise = require('sequelize').Promise
 
 var seedUsers = function () {
@@ -41,11 +44,85 @@ var seedUsers = function () {
   return Promise.all(creatingUsers)
 }
 
+var seedProducts = function () {
+  var products = [
+    {
+      price: 25.99,
+      category: 'Totes',
+      pictureUrl: 'http://i3.cpcache.com/product/643931876/i_love_hipsters_tote_bag.jpg?width=225&height=225&Filters=%5B%7B%22name%22%3A%22background%22%2C%22value%22%3A%22F2F2F2%22%2C%22sequence%22%3A2%7D%5D',
+      title: 'Mai Glasses - Tote',
+      information: "A revolutionary tote, sporting some trendy glasses that'll let people know that you woke af.",
+      availability: true
+    },
+    {
+      price: 10.99,
+      category: 'Totes',
+      pictureUrl: 'https://www.zazzle.com/rlv/hipster_tote_bag-r101af6cd5c864d6fafd2ef3872f06cb1_v9w6h_8byvr_324.jpg',
+      title: "I'm a Hipster - Tote",
+      information: "Don't have good enough style to be a hipster? Just tell people you are one.",
+      availability: true
+    }
+  ]
+
+  var creatingProducts = products.map(function (productObj) {
+    return Product.create(productObj)
+  })
+
+  return Promise.all(creatingProducts)
+}
+
+var seedOrders = function () {
+  var orders = [
+    {
+      status: 'purchased'
+    },
+    {
+      status: 'pre-purchase'
+    }
+  ]
+
+  var creatingOrders = orders.map(function (orderObj) {
+    return Order.create(orderObj)
+  })
+
+  return Promise.all(creatingOrders)
+}
+
+var seedCarts = function () {
+  var carts = [
+    {
+      myItems: [1]
+    },
+    {
+      myItems: []
+    }
+  ]
+
+  var creatingCarts = carts.map(function (cartObj) {
+    return Cart.create(cartObj)
+  })
+
+  return Promise.all(creatingCarts)
+}
+
 db.sync({ force: true })
   .then(function () {
     return seedUsers()
   })
   .then(function () {
+    console.log(chalk.yellow('Users seeded...'))
+    return seedProducts()
+  })
+  .then(function () {
+    console.log(chalk.yellow('Products seeded...'))
+    return seedOrders()
+  })
+  .then(function () {
+    console.log(chalk.yellow('Orders seeded...'))
+    return seedCarts()
+  })
+  .then(function () {
+    console.log(chalk.yellow('Carts seeded...'))
     console.log(chalk.green('Seed successful!'))
     process.exit(0)
   })
