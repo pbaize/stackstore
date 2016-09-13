01e02cea6bb9a0aadab3ec67ffb1bcde3cf86dc2
Tony <yechao@umich.edu>
2016-09-13 11:30:24 -0400

    productFactory has fetchAll and fetchById method version 2

--- browser/js/common/factories/products.factory.js
+++ browser/js/common/factories/products.factory.js
@@ -1,5 +1,32 @@
 /* global app */
-app.factory('ProductsFactory', function ($http, $log) {
- let cachProduct = [];
+app.factory('ProductsFactory', function ($http, $log, $q) {
+  let cachProduct = []
+  let ProductsFactory = {}
 
+  ProductsFactory.fetchAll = function () {
+    return $http.get('/api/products')
+      .then(function (response) {
+        let allProducts = response.data
+        return allProducts
+      }).catch($log.error)
+  }
+
+  ProductsFactory.fetchById = function (id) {
+    if (cachProduct[id] === undefined) {
+      return $http.get('/api/products/' + id)
+        .then(function (response) {
+          console.log('loading new product')
+          let oneProduct = response.data
+          cachProduct[id] = oneProduct // put the result to cach
+          return oneProduct
+        }).catch($log.error)
+    } else {
+      console.log('using cach result')
+      let deferred = $q.defer()
+      deferred.resolve(cachProduct[id])
+      return deferred.promise
+    }
+  }
+
+  return ProductsFactory
 })
