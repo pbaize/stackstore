656ccd33bba8b8889a8614869441d0263fcc73b9
devpatel91 <devnitinpatel91@gmail.com>
2016-09-13 12:04:39 -0400

    Routes made for products

---
+++ server/app/routes/api/products.js
@@ -0,0 +1,26 @@
+'use strict'
+var router = require('express').Router()
+var Products = require('../../../db/models/product.js')
+// eslint-disable-line new-cap
+module.exports = router
+
+router.get('/', function (req, res, next) {
+  Products.findAll()
+    .then(function (products) {
+      res.status(200).send(products)
+    })
+    .catch(next)
+})
+
+router.get('/:id', function (req, res, next) {
+  var id = req.params.id
+
+  Products.findById(id)
+    .then(function (products) {
+      if (products === null) {
+        return res.send(404).end()
+      }
+      res.status(200).send(products)
+    })
+    .catch(next)
+})
