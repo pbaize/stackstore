78e4216514e81db5bd18dc630ffcb7279e2bad21
Eliot S <eliotpszw@gmail.com>
2016-09-15 07:39:33 -0400

    update

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -7,7 +7,8 @@ var db = require('../_db')
 
 module.exports = db.define('user', {
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    unique: true
   },
   password: {
     type: Sequelize.STRING
