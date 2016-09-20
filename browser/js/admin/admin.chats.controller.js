dc2eb818876d39ea99c3ec930c92d95fd8b6c487
Eliot S <eliotpszw@gmail.com>
2016-09-20 18:32:16 -0400

    we got ourselves a sketchy chat boys

--- browser/js/admin/admin.chats.controller.js
+++ browser/js/admin/admin.chats.controller.js
@@ -3,9 +3,10 @@
 app.controller('AdminChatCtrl', function ($scope, $rootScope, $state, ChatFactory) {
   $scope.allLiveDiscussions = []
   $scope.currentConversation = []
+  $scope.currentSelection = {user: null}
 
   $scope.fetchChats = function () {
-    console.log('ADMIN: Attemptng to authenticate and fetch live chats.')
+    console.log('ADMIN: Attempting to authenticate and fetch live chats.')
     ChatFactory.findMyId()
       .then(function (myId) {
         $rootScope.socket.emit('adminauth', {id: myId})
@@ -25,7 +26,24 @@ app.controller('AdminChatCtrl', function ($scope, $rootScope, $state, ChatFactor
     $scope.currentConversation = aChat.chatHistory.map(function (a) {
       return {message: a.message, user: a.user, timestamp: a.timestamp}
     })
-    console.log($scope.currentConversation)
+    $scope.currentSelection.user = aChat.userName
     $scope.$evalAsync()
+  }
+
+  $scope.sendAdMessage = function (message) {
+    if ($scope.currentSelection) {
+      console.log('ADMIN: Attempting to authenticate and send message to proper user.')
+      ChatFactory.findMyId()
+        .then(function (myId) {
+          $rootScope.socket.emit('adminmessage', {id: myId, user: $scope.currentSelection.user, message: {message: message, timestamp: new Date(), user: 'Another Hipster'}})
+          $scope.currentConversation.push({message: message, timestamp: new Date(), user: 'You'})
+          $scope.thechat.$setPristine()
+          $scope.msg = null
+          $scope.$evalAsync()
+          console.log('ADMIN: Sending message up.')
+        })
+    } else {
+      console.log('Must select a conversation first!')
+    }
   }
 })
